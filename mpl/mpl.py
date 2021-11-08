4947a97e078547ea38332784861378bd6d447c50
Calvin Leung <calvinl@mit.edu>
2021-11-08 16:34:07 -0500

    renamed get_size to figsize

--- mpl/mpl.py
+++ mpl/mpl.py
@@ -7,7 +7,7 @@ plt.style.use(path_to_style)
 journal_textwidths = {'prd': 6.50127,
                       'none': 8.5} # Latex's \textwidth in inches
 
-def get_size(fraction_of_textwidth = 0.45,height_per_width  = 1 / 1.61803399,journal = 'prd'):
+def figsize(fraction_of_textwidth = 0.45,height_per_width  = 1 / 1.61803399,journal = 'prd'):
     textwidth = journal_textwidths[journal]
     width_in = textwidth * fraction_of_textwidth
     height_in = height_per_width * width_in
