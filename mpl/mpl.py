4cc7361429fcfeac4d6109a8e23084492749ffbf
Calvin Leung <42367504+leungcalvin@users.noreply.github.com>
2023-03-30 11:39:43 -0400

    Update mpl.py
    CL: add AJ and ApJ

--- mpl/mpl.py
+++ mpl/mpl.py
@@ -6,10 +6,13 @@ plt.style.use(path_to_style)
 
 journal_textwidths = {'prd': 6.50127,
                       'none': 8.5,
-                      'kaobook':6.5 # just a guess
+                      'kaobook':6.5, # just a guess
+                      'apj':6.0, # from \textwidth in aastex631.cls
+                      'aj': 6.0, # from \textwidth in aastex631.cls
                       } # Latex's \textwidth in inches
 
 def figsize(fraction_of_textwidth = 0.45,height_per_width  = 1 / 1.61803399,journal = 'prd'):
+    """When making your figure, decide its aspect ratio and your journal and its width as a fraction of the \textwidth of the journal. Then use matplotlib.pyplot.Figure(figsize = ...) to specify the right size"""
     textwidth = journal_textwidths[journal]
     width_in = textwidth * fraction_of_textwidth
     height_in = height_per_width * width_in
